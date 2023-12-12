4fa52d77c0fb34b872789daba631bb5a0148f87d
Margarita <sempai_m@mail.ru>
2023-12-12 23:59:20 +0300

    .

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Logo from "../../img/Logo.svg";
 import classes from "./header.module.scss";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 const Header = () => {
-  const Active = useSelector((state) => state.checkbox.checkbox); // извлекаем фильтр из состояния Redux
+  // const Active = useSelector((state) => state.checkbox.checkbox); // извлекаем фильтр из состояния Redux
   return (
     <div className={classes.header}>
       <img src={Logo} alt="Logo" />
-      <h1>{Active}</h1>
+      {/* <h1>{Active}</h1> */}
     </div>
   );
 };
--- src/store/checkboxSlice.jsx
+++ src/store/checkboxSlice.jsx
@@ -1,13 +1,67 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { checkbox: "" };
+export const initialState = {
+  all: false,
+  no_transfer: false,
+  one_transfer: false,
+  two_transfer: false,
+  tree_transfer: false,
+};
 
-const checkboxSlice = createSlice({
+export const checkboxSlice = createSlice({
   name: "checkbox",
-  initialState,
+  initialState: initialState,
   reducers: {
     checkboxState: (state, action) => {
-      state.checkbox = action.payload;
+      const checkbox = action.payload;
+
+      if (checkbox === "Все") {
+        state.all = !state.all;
+
+        if (state.all === true) {
+          state.no_transfer = true;
+          state.one_transfer = true;
+          state.two_transfer = true;
+          state.tree_transfer = true;
+        } else {
+          state.no_transfer = false;
+          state.one_transfer = false;
+          state.two_transfer = false;
+          state.tree_transfer = false;
+        }
+      // } else {
+      //   // Если любая другая галочка снимается при включенной галочке "Все"
+      //   state.all = false;
+
+        // Если все галочки включены, проставляем галочку "Все"
+        //     if (
+        //       state.no_transfer &&
+        //       state.one_transfer &&
+        //       state.two_transfer &&
+        //       state.tree_transfer
+        //     ) {
+        //       state.all = true;
+        //     }
+        //   }
+        // },
+        // toggleFilter: (state, action) => {
+        //   // Переключение состояния отдельного фильтра
+        //   const filterName = action.payload;
+        //   state[filterName] = !state[filterName];
+
+        //   // Если все галочки включены, проставляем галочку "Все"
+        //   if (
+        //     state.no_transfer &&
+        //     state.one_transfer &&
+        //     state.two_transfer &&
+        //     state.tree_transfer
+        //   ) {
+        //     state.all = true;
+        //   } else {
+        //     // Если любая галочка снимается, снимаем галочку "Все"
+        //     state.all = false;
+      }
+      // return state
     },
   },
 });
