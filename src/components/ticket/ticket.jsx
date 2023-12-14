1912c1f6af8721a3fec89722e24fc7a89774d1b0
Margarita <sempai_m@mail.ru>
2023-12-15 02:59:03 +0300

    .

--- src/components/ticket/ticket.jsx
+++ src/components/ticket/ticket.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import classes from "../ticket/ticket.module.scss";
 import Logo from "../../img/S7-Logo.svg";
-// import { format } from 'date-fns';
 
 const Ticket = ({ ticket }) => {
-  const formattedPrice = ticket.price.toLocaleString("ru-RU");
+  const formatPrice = ticket.price.toLocaleString("ru-RU");
 
-  const formattedTime = (ticket, num) => {
+  const formatTime = (ticket, num) => {
     const minutes = ticket.segments[num].duration;
     const hours = Math.floor(minutes / 60);
     const remainingMinutes = minutes % 60;
-    return hours + "ч " + remainingMinutes + "м";
+    return { hours: hours, minutes: remainingMinutes };
   };
 
+  const formatDate = (ticket, num, { hours, minutes }) => {
+    const date = new Date(ticket.segments[num].date);
 
+    const hoursDate = date.getUTCHours();
+    const minutesDate = date.getUTCMinutes();
+    
+    date.setHours(date.getHours() + hours); 
+    date.setMinutes(date.getMinutes() + minutes); 
+    const formattedHours = (date.getUTCHours() < 10 ? "0" : "") + date.getUTCHours();
+    const formattedMinutes = (date.getUTCMinutes() < 10 ? "0" : "") + date.getUTCMinutes();
+    return `${hoursDate}:${minutesDate} - ${formattedHours}:${formattedMinutes}`;
+  };
 
   return (
     <div className={classes.ticket}>
       <div className={classes.ticket_header}>
-        <div className={classes.price}>{formattedPrice + " Р"}</div>
+        <div className={classes.price}>{formatPrice + " Р"}</div>
         <div className={classes.logo}>
           <img src={Logo} alt="Logo" />
         </div>
@@ -27,32 +37,36 @@ const Ticket = ({ ticket }) => {
       <div className={classes.ticket_data_wrapper}>
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>MOW – HKT</p>
-          <p>{ticket.segments[0].date}</p>
+          <p>{formatDate(ticket, 0, formatTime(ticket, 0))}</p>
         </div>
 
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>В пути</p>
-          <p className={classes.lowercase}>{formattedTime(ticket, 0)}</p>
+          <p className={classes.lowercase}>{`${formatTime(ticket, 0).hours}ч ${
+            formatTime(ticket, 0).minutes
+          }м`}</p>
         </div>
 
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>2 пересадки</p>
-          <p>{ticket.segments[0].stops.join(', ')}</p>
+          <p>{ticket.segments[0].stops.join(", ")}</p>
         </div>
 
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>MOW – HKT</p>
-          <p>{ticket.segments[1].date}</p>
+          <p>{formatDate(ticket, 0, formatTime(ticket, 0))}</p>
         </div>
 
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>В пути</p>
-          <p className={classes.lowercase}>{formattedTime(ticket, 1)}</p>
+          <p className={classes.lowercase}>{`${formatTime(ticket, 0).hours}ч ${
+            formatTime(ticket, 0).minutes
+          }м`}</p>
         </div>
 
         <div className={classes.ticket_data}>
           <p className={classes.ticket_data_block__title}>1 пересадка</p>
-          <p>{ticket.segments[1].stops.join(', ')}</p>
+          <p>{ticket.segments[1].stops.join(", ")}</p>
         </div>
       </div>
     </div>
