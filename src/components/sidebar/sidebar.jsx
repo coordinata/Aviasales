4f392e8628749d225c9d6a67bad89bbdc22948e0
Margarita <sempai_m@mail.ru>
2023-12-16 17:44:42 +0300

    .

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./sidebar.module.scss";
 
 const Sidebar = () => {
@@ -8,64 +8,104 @@ const Sidebar = () => {
   const [twoTransfer, setTwoTransfer] = useState(false);
   const [treeTransfer, setTreeTransfer] = useState(false);
 
+  const handleTransferChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.checked;
+
+    switch (name) {
+      case "allTransfer":
+        setAllTransfer(value);
+        setNoTransfer(value);
+        setOneTransfer(value);
+        setTwoTransfer(value);
+        setTreeTransfer(value);
+        break;
+      case "noTransfer":
+        setNoTransfer(value);
+        break;
+      case "oneTransfer":
+        setOneTransfer(value);
+        break;
+      case "twoTransfer":
+        setTwoTransfer(value);
+        break;
+      case "treeTransfer":
+        setTreeTransfer(value);
+        break;
+      default:
+        break;
+    }
+
+    if (!value) {
+      setAllTransfer(false);
+    } else {
+      if (noTransfer && oneTransfer && twoTransfer && treeTransfer) {
+        setAllTransfer(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (noTransfer && oneTransfer && twoTransfer && treeTransfer) {
+      setAllTransfer(true);
+    } else {
+      setAllTransfer(false);
+    }
+  }, [noTransfer, oneTransfer, twoTransfer, treeTransfer]);
+
   return (
     <div className={classes.sidebar}>
       <h3>Количество пересадок</h3>
       <form className={classes.form}>
-        <label
-          className={classes.label}
-          onClick={() => setAllTransfer(!allTransfer)}
-        >
+        <label className={classes.label}>
           <input
             checked={allTransfer}
             type="checkbox"
+            name="allTransfer"
             className={classes.input_class}
+            onChange={handleTransferChange}
           ></input>
           <span className={classes.checkbox}></span>
           Все
         </label>
-        <label
-          className={classes.label}
-          onClick={() => setNoTransfer(!noTransfer)}
-        >
+        <label className={classes.label}>
           <input
             checked={noTransfer}
             type="checkbox"
+            name="noTransfer"
             className={classes.input_class}
+            onChange={handleTransferChange}
           ></input>
           <span className={classes.checkbox}></span>
           Без пересадок
         </label>
-        <label
-          className={classes.label}
-          onClick={() => setOneTransfer(!oneTransfer)}
-        >
+        <label className={classes.label}>
           <input
             checked={oneTransfer}
             type="checkbox"
+            name="oneTransfer"
             className={classes.input_class}
+            onChange={handleTransferChange}
           ></input>
           <span className={classes.checkbox}></span>1 пересадка
         </label>
-        <label
-          className={classes.label}
-          onClick={() => setTwoTransfer(!twoTransfer)}
-        >
+        <label className={classes.label}>
           <input
             checked={twoTransfer}
             type="checkbox"
+            name="twoTransfer"
             className={classes.input_class}
+            onChange={handleTransferChange}
           ></input>
           <span className={classes.checkbox}></span>2 пересадки
         </label>
-        <label
-          className={classes.label}
-          onClick={() => setTreeTransfer(!treeTransfer)}
-        >
+        <label className={classes.label}>
           <input
             checked={treeTransfer}
             type="checkbox"
+            name="treeTransfer"
             className={classes.input_class}
+            onChange={handleTransferChange}
           ></input>
           <span className={classes.checkbox}></span>3 пересадки
         </label>
@@ -73,5 +113,4 @@ const Sidebar = () => {
     </div>
   );
 };
-
 export default Sidebar;
